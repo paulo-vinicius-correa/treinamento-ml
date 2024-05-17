7e530e9cc1cf96caf6d912384436db4377a728f4
Paulo Vinicius <paulo.correa1501@hotmail.com>
2024-05-17 10:33:45 -0300

    :sparkles: feat: consulta Safra Criada

---
+++ safra.sql
@@ -0,0 +1,14 @@
+SELECT t1.*,
+       CASE WHEN t2.seller_id IS NOT NULL THEN 1 ELSE 0 END AS flag_model 
+FROM tb_book_sellers AS t1
+LEFT JOIN (
+    SELECT DISTINCT 
+        t2.seller_id
+    FROM tb_orders AS t1
+    LEFT JOIN tb_order_items AS t2
+    ON t1.order_id = t2.order_id
+    WHERE t1.order_approved_at BETWEEN '2017-04-01' 
+    AND '2017-07-01'
+    AND t1.order_status = 'delivered'
+) AS t2
+ON t1.id_vendedor = t2.seller_id
