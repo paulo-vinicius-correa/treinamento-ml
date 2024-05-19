f77e1c1133360eb73e732286ece5d2b02d9aed01
Paulo Vinicius <paulo.correa1501@hotmail.com>
2024-05-19 14:12:10 -0300

    :sparkles: Finalizando a ABT  (ANALYTICAL BASE TABLE)

--- sql/var_resposta.sql
+++ sql/var_resposta.sql
@@ -1,21 +1,35 @@
-SELECT t1.dt_ref,
-       t1.seller_id,
-       MAX(COALESCE(t2.venda, 0)) AS flag_venda
-FROM tb_book_sellers AS t1
-LEFT JOIN (
+SELECT
+    t2.*,
+    t1.flag_venda
+    
+FROM (
     SELECT 
-        strftime('%Y%m', order_approved_at) as dt_venda,
-        t2.seller_id,
-        MAX(1) AS venda
-    FROM tb_orders AS t1
-    LEFT JOIN tb_order_items AS t2
-    ON t1.order_id = t2.order_id
-    WHERE t1.order_approved_at IS NOT NULL
-    AND t2.seller_id IS NOT NULL
-    AND t1.order_status = 'delivered'
-    GROUP BY strftime('%Y%m', order_approved_at), t2.seller_id
-) AS t2
-ON t1.seller_id = t2.seller_id
-AND t2.dt_venda BETWEEN strftime('%Y%m', t1.dt_ref) AND strftime('%Y%m', DATE(t1.dt_ref, '+2 MONTHS'))
-GROUP BY t1.dt_ref, t1.seller_id
-ORDER BY t1.dt_ref;
+        t1.dt_ref,
+        t1.seller_id,
+        MAX(COALESCE(t2.venda, 0)) AS flag_venda
+    FROM 
+        tb_book_sellers AS t1
+    LEFT JOIN (
+        SELECT 
+            strftime('%Y%m', t1.order_approved_at) AS dt_venda,
+            t2.seller_id,
+            1 AS venda
+        FROM 
+            tb_orders AS t1
+        LEFT JOIN 
+            tb_order_items AS t2 ON t1.order_id = t2.order_id
+        WHERE 
+            t1.order_approved_at IS NOT NULL
+            AND t2.seller_id IS NOT NULL
+            AND t1.order_status = 'delivered'
+        GROUP BY 
+            strftime('%Y%m', t1.order_approved_at), t2.seller_id
+    ) AS t2 ON t1.seller_id = t2.seller_id
+            AND t2.dt_venda BETWEEN strftime('%Y%m', t1.dt_ref) AND strftime('%Y%m', DATE(t1.dt_ref, '+2 MONTHS'))
+    GROUP BY 
+        t1.dt_ref, t1.seller_id
+    ORDER BY 
+        t1.dt_ref
+) AS t1
+LEFT JOIN 
+    tb_book_sellers AS t2 ON t1.seller_id = t2.seller_id AND t1.dt_ref = t2.dt_ref;
